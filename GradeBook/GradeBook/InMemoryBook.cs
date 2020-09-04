5e83aa01e4fe485fb30e3c0154d71fd66282bdcc
Peter <petertran198@gmail.com>
2020-09-04 15:30:11 -0500

    Renamed Book class to InMemoryBook and made an abstract class called Book that contains functionalites that every book should have

---
+++ GradeBook/GradeBook/InMemoryBook.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    // convention to build a delegate to define an event u send a sender(person is sending this event) of type object
+    // Second is some form of event argument and we call it args 
+    // Also notice that this is not inside of InMemoryBook class but inside of namespace GradeBook
+    public delegate void GradeAddedDelegate( object sender, EventArgs args);
+
+
+    public class InMemoryBook : Book
+    {
+        // grades is a private field and the convention is to make it lowercase 
+        List<double> grades;
+        // Readonly field can only be declared once, either initilization or in the constructor
+        public readonly string Category;
+        // Defining a field of type GradeAddedDelegate in InMemoryBook class
+        // By adding the keyword event this adds restriction to the delegate. You can not assign it to method but you can add method to it
+        // In another work .GradeAdded can only appear on the left hand side of += or -= 
+
+        public event GradeAddedDelegate GradeAdded;
+        public InMemoryBook(string name, string category="N/A") 
+        {
+            // You have to instantiate it to create the list of grades or it will give a null exception because of the grades field has not been instantiate
+            grades = new List<double>();
+            Name = name;
+            Category = category;
+
+        }
+
+        public override void AddGrade(double grade) /*----------Overloaded method---------- */
+        {
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                // We check if GradeAdded is populated if it isnt than that means no one is listening or added a method reference into the delegate
+                if(GradeAdded != null)
+                {
+                    // if someone did added a method reference than we invoke the delegate 
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else//If this is not run then it will throw an argument exception
+            {
+                Console.WriteLine($"Grade of {grade} was not added must be within 1-100");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+
+
+        public void AddGrade(char letter)/*----------- Overloaded method ----------*/
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                case 'F':
+                    AddGrade(50);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+        // Return a instance of Statistics object that contains highest, lowest, and average grades
+        public Statistics GetStatistics()
+        {
+            Statistics result = new Statistics();
+            result.Average = 0.0;
+            result.HighestGrade = double.MinValue;
+            result.LowestGrade = double.MaxValue;
+            foreach (var grade in grades)
+            {
+                // finds the highest number of the two and assign it to result.High 
+                result.HighestGrade = Math.Max(grade, result.HighestGrade);
+                result.LowestGrade = Math.Min(grade, result.LowestGrade);
+                result.Average += grade;
+            }
+
+
+            result.Average /= grades.Count;
+            switch (result.Average)
+            {
+                case var grade when (grade >= 90.0 && grade <= 100):
+                    result.LetterGrade = 'A';
+                    break;
+                case var grade when (grade >= 80.0 && grade < 90.0):
+                    result.LetterGrade = 'B';
+                    break; 
+                case var grade when (grade >= 70 && grade < 80):
+                    result.LetterGrade = 'C';
+                      break;
+                case var grade when (grade >= 60.0 && grade < 70):
+                    result.LetterGrade = 'D';
+                    break;
+                default:
+                    result.LetterGrade = 'F';
+                    break;
+            }
+            return result; 
+
+
+        }
+
+
+    }
+}
--- GradeBook/GradeBook/Program.cs
+++ GradeBook/GradeBook/Program.cs
@@ -8,19 +8,19 @@ namespace GradeBook
     {
         static void OnGradeAdded(object sender, EventArgs e)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine($"-------- Added grade -------");
         }
 
         static void Main(string[] args)
         {
 
-            Book book = new Book(name: "First Math book", category: "Math");
+            InMemoryBook book = new InMemoryBook(name: "First Math book", category: "Math");
             // Added OnGradeAdded method into the book.GradeAdded delegate 
             //Since this is an event delegate you can += or -= 
             // but you cant do something like book.GradeAdded = null 
             // like a pure delegate 
             book.GradeAdded += OnGradeAdded;
-            book.InputGrade();
+            EnterGrade(book);
 
             Statistics stats = book.GetStatistics();
             Console.WriteLine($"For the book {book.Name} in {book.Category} Category");
@@ -28,6 +28,35 @@ namespace GradeBook
             Console.WriteLine($"The highest grade is {stats.HighestGrade}");
             Console.WriteLine($"The lowest grade is {stats.LowestGrade}");
 
+        }
+        
+        // Takes in any type of book and allow you to enter grades
+        public static void EnterGrade(Book book)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a Grade, press 'q' you are done");
+                string gradeString = Console.ReadLine();
+                if (gradeString == "q")
+                {
+                    Console.WriteLine("Grade has been inputed");
+                    break;
+                }
+                try
+                {
+                    var grade = double.Parse(gradeString);
+                    book.AddGrade(grade);
+                }// One try block can have multiple catch for different exception
+                catch (ArgumentException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
